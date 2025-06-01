a5ef3043b6954c37658f5a7591fd7c5232dc03a2
Srikanth <aryan.srikanth@rwth-aachen.de>
2025-06-01 19:20:49 +0200

    Files modified

---
+++ assets/js/firebase-init.js
@@ -0,0 +1,12 @@
+// firebase-init.js
+const firebaseConfig = {
+  apiKey: "AIzaSyBpTPISNivXTRPqYiB2oO8LJmI6J7DONx0",
+  authDomain: "fem-hub-comments.firebaseapp.com",
+  projectId: "fem-hub-comments",
+  storageBucket: "fem-hub-comments.firebasestorage.app",
+  messagingSenderId: "464341647492",
+  appId: "1:464341647492:web:60a2f87414b7108f7942d1"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
