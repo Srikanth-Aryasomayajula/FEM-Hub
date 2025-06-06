8081dce7436d116fade97c15a4c44a07f0fe0ffd
Srikanth <aryan.srikanth@rwth-aachen.de>
2025-06-07 01:39:54 +0200

    Files modified

--- assets/js/main.js
+++ assets/js/main.js
@@ -587,7 +587,7 @@ async function renderComment(comment, allComments, indent = 0) {
   wrapper.innerHTML = `
     <div class="comment-header">
       <span class="comment-name">${comment.name}</span>
-      <span class="comment-time">${formatDateToDDMMMYYYY(comment.timestamp.toDate())}, ${new Date(comment.timestamp.toDate()).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true })}</span>
+      <div class="comment-time">${formatDateToDDMMMYYYY(comment.timestamp.toDate())}, ${new Date(comment.timestamp.toDate()).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true })}</div>
     </div>
     <div class="comment-text">${comment.text}</div>
     <div class="comment-actions">
